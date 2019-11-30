0fc839aa2877ed5af42cdf0250f93b151f5f5aa2
Vladimir <volkvid@yandex.ru>
2019-11-30 18:34:59 +0300

    добавить бота

--- tga/tga/settings.py
+++ tga/tga/settings.py
@@ -108,3 +108,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Настройки бота
+
+TOKEN = '855883347:AAHxvaX7DSchTqeXA59q3yqbQw74ZHixVM4'
+
+PROXY_URL = 'https://telegg.ru/orig/bot'
